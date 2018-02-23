49310a3f865b2877410091aed794ed1f7dbde266
tilatou <36776507+tilatou@users.noreply.github.com>
2018-02-23 22:55:13 +0100

    Add files via upload

---
+++ app/js/app.js
@@ -0,0 +1,1 @@
+var supApp=angular.module('supApp',['ngRoute']);
---
+++ app/js/controllers/demoCtrl.js
@@ -0,0 +1,7 @@
+supApp.controller('demoCtrl',['scope', function($scope){
+	$scope.text="ceci est un texte";
+	$scope.student=[
+	          {firstName="toto",lastName="tata"},     
+	          {firstName="toto1",lastName="tata1"}      
+	                ];
+}]);
